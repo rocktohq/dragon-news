9dfe69cd5a86705d240e58f8b88d7b1cf1235788
Saidul Mursalin <itzmonir@gmail.com>
2023-10-04 03:36:12 +0600

    Latest News Updated

--- src/components/LatestNews/LatestNews.jsx
+++ src/components/LatestNews/LatestNews.jsx
@@ -8,7 +8,7 @@ const LatestNews = ({ latestHeadLines }) => {
       <button className="bg-latestNews rounded-none text-white font-medium text-xl px-6 py-2">Latest</button>
       <Marquee pauseOnHover={true} speed={100} style={{ fontSize: "18px", fontWeight: 600 }}>
         {
-          latestHeadLines.map(latestHeadLine => <Link to={`/news/${latestHeadLine._id}`} key={latestHeadLine._id}><p className="mx-1">{latestHeadLine.title}</p></Link>)
+          latestHeadLines?.map(latestHeadLine => <div className="flex items-center" key={latestHeadLine._id}><Link to={`/news/${latestHeadLine._id}`}><p className="hover:text-orange-500">{latestHeadLine.title}</p></Link><span className="mx-3 text-gray-400">||</span></div>)
         }
       </Marquee>
     </div>
